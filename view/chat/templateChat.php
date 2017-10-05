a53571a82acd86f88b423c60f54ca6c9c3124323
Lenny4 <alexandre.beaujour@devinci.fr>
2017-10-05 08:30:49 +0200

    Display the username of the user when chat

--- view/chat/templateChat.php
+++ view/chat/templateChat.php
@@ -1,9 +1,21 @@
+<?php
+include_once("../../config/config.php");
+
+$id_other_user=$_POST['id'];
+$datas = $bdd->prepare("SELECT * FROM `user` WHERE id=?");
+$datas->execute(array($id_other_user));
+$username="";
+while ($data = $datas->fetch()) {
+    $username=$data['username'];
+}
+?>
+
 <div id="<?= $_POST['id']; ?>" class="row chat-window newChat col-xs-3" style="left: 0px;">
     <div class="col-xs-12" style="padding-left: 0px;">
         <div class="panel panel-default">
             <div class="panel-heading top-bar">
                 <div class="col-md-8 col-xs-8">
-                    <h3 class="panel-title"><span class="glyphicon glyphicon-comment"></span> Chat - Miguel</h3>
+                    <h3 class="panel-title"><span class="glyphicon glyphicon-comment"></span> <?= $username; ?></h3>
                 </div>
                 <div class="col-md-4 col-xs-4" style="text-align: right;">
                     <a href="#"><span id="minim_chat_window" class="glyphicon glyphicon-minus icon_minim"></span></a>
