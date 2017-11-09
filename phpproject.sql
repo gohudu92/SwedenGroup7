0390b18ed97e3c912182b32647bbaa70ec1a6189
Lenny4 <alexandre.beaujour@hotmail.fr>
2017-11-09 10:02:31 +0100

    update database

--- phpproject.sql
+++ phpproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  lun. 30 oct. 2017 à 14:30
+-- Généré le :  jeu. 09 nov. 2017 à 10:01
 -- Version du serveur :  10.1.28-MariaDB
 -- Version de PHP :  7.1.10
 
@@ -25,6 +25,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(9, 'country'),
+(10, 'sky');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `love`
+--
+
+CREATE TABLE `love` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `love`
+--
+
+INSERT INTO `love` (`id`, `user_id`, `post_id`) VALUES
+(48, 5, 53);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `messages`
 --
 
@@ -37,6 +89,14 @@ CREATE TABLE `messages` (
   `view` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Déchargement des données de la table `messages`
+--
+
+INSERT INTO `messages` (`id`, `id_receiver`, `id_sender`, `content`, `time`, `view`) VALUES
+(8, 6, 5, 'ok', 1510214880, 1),
+(9, 5, 6, 'ouais', 1510214885, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +108,42 @@ CREATE TABLE `online` (
   `id_user` int(11) NOT NULL,
   `time` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `online`
+--
+
+INSERT INTO `online` (`id`, `id_user`, `time`) VALUES
+(1, 0, 1509369997),
+(2, 1, 1509370328),
+(3, 2, 1509370134),
+(4, 3, 1510205253),
+(5, 4, 1510203041),
+(6, 5, 1510218113),
+(7, 6, 1510215045);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `post`
+--
+
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `time` int(11) NOT NULL,
+  `love` int(11) NOT NULL,
+  `category_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `post`
+--
+
+INSERT INTO `post` (`id`, `user_id`, `content`, `time`, `love`, `category_name`) VALUES
+(53, 5, '<p>I talk about country</p>', 1510217110, 1, 'country'),
+(54, 5, '<p>I talk about sky</p>', 1510217142, 0, 'sky');
 
 -- --------------------------------------------------------
 
@@ -64,8 +160,34 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `mail`, `image`, `password`) VALUES
+(5, 'Emile', '1fiuhf@mail.fr', '', '356a192b7913b04c54574d18c28d46e6395428ab'),
+(6, 'Leon', 'fdhfsk@mail.fr', '', '356a192b7913b04c54574d18c28d46e6395428ab');
+
+--
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `love`
+--
+ALTER TABLE `love`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `messages`
@@ -80,6 +202,12 @@ ALTER TABLE `online`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `user`
 --
 ALTER TABLE `user`
@@ -90,22 +218,46 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT pour la table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT pour la table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT pour la table `love`
+--
+ALTER TABLE `love`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+
+--
 -- AUTO_INCREMENT pour la table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `online`
 --
 ALTER TABLE `online`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
